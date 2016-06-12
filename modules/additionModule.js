76b006b40a098bb45904b38e3cb2d74e84a9cc1e
Craig <Craig@Craigs-MacBook-Pro-2.local>
2016-06-12 12:22:21 -0500

    third commit, math is working

---
+++ modules/additionModule.js
@@ -0,0 +1,22 @@
+
+var answer = function (incommingObject) {
+  console.log("From server: " + incommingObject);
+  
+  if (incommingObject.select == "add") {
+ return Number(incommingObject.first) + Number(incommingObject.second);
+  }
+  else if(incommingObject.select == "subtract") {
+ return Number(incommingObject.first) - Number(incommingObject.second);
+  }
+  else if(incommingObject.select == "multiply") {
+ return Number(incommingObject.first) * Number(incommingObject.second);
+  }
+  else if(incommingObject.select == "divide") {
+ return Number(incommingObject.first) / Number(incommingObject.second);
+  }
+   else {
+    return "Dude, that's impossible with this calulator.";
+  }
+};
+
+module.exports = answer;
---
+++ server/app.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+var bodyParser=require('body-parser');
+var urlencodedParser = bodyParser.urlencoded( { extended: false } );
+
+var answer = require("../modules/additionModule.js");
+
+// TAKE OUT LOCAL HOST FOR HEROKU!!!!
+var server = app.listen (3000, 'localhost', function(){
+  var port = server.address().port;
+  console.log("Hello from server 3000");
+});
+// server tested and works
+
+app.get("/", function(req, res){
+ res.sendFile(path.resolve("views/index.html"));
+});
+
+  app.post('/calculate', urlencodedParser, function( req, res ){
+  res.write( "post received something: " + answer(req.body));
+  res.end();
+});
+
+app.use(express.static("public"));
